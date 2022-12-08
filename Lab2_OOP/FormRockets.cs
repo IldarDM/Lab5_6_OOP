f8c8da1e53bc8efb4209105ddd2e844bc54ea58c
kep_ildar <i.davletyarov@gmail.com>
2022-12-08 10:15:07 +0300

    Добавьте файлы проекта.

---
+++ Lab2_OOP/FormRockets.cs
@@ -0,0 +1,159 @@
+﻿using MyFigures;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5_OOP
+{
+    public partial class FormRockets : Form
+    {
+        private Button but;
+        public FormRockets(Button but)
+        {
+            InitializeComponent();
+            this.but = but;
+            for (int i = 0; i < FiguresContainer.RocketsList.Count; i++)
+            {
+                figure_box.Items.Add(FiguresContainer.RocketsList[i]);
+                figure_box.Items[i] = $"Rocket{FiguresContainer.RocketsList[i].number}";
+            }
+            buttonDelete.Enabled = false;
+            Button_New_Cords.Enabled = false;
+            Button_New_Size.Enabled = false;
+            Start_button.Enabled = false;
+        }
+        private void FormRockets_MouseDown(object sender, MouseEventArgs e)
+        {
+            base.Capture = false;
+            Message m = Message.Create(base.Handle, 0xa1, new IntPtr(2), IntPtr.Zero);
+            this.WndProc(ref m);
+        }
+
+        private void button_back_Click(object sender, EventArgs e)
+        {
+            Close();
+            but.Enabled = true;
+        }
+
+        private void Button_Draw_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(setX.Text, out int number) && int.TryParse(setY.Text, out number) &&
+                int.TryParse(width.Text, out number) && int.TryParse(height.Text, out number))
+            {
+                int x = int.Parse(setX.Text);
+                int y = int.Parse(setY.Text);
+                int width_f = int.Parse(width.Text);
+                int height_f = int.Parse(height.Text);
+                if (!(width_f < 0 || height_f < 0))
+                {
+                    if (!(x < 0 || y < 0 || x + width_f > Figure.pictureBox.Width || y + height_f > Figure.pictureBox.Height))
+                    {
+                        Rocket Rock = new Rocket(x, y, width_f, height_f);
+                        Rock.Draw();
+                        figure_box.Items.Add(Rock);
+                        figure_box.Items[figure_box.FindStringExact(Rock.ToString())] = $"Rock{Rock.number}";
+                    }
+                    else
+                    {
+                        MessageBox.Show("Фигура должна полностью помещаться на холст");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("У фигуры должна быть положительная длина и высота");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Некорректный формат ввода");
+            }
+        }
+
+        private void Button_New_Cords_Click(object sender, EventArgs e)
+        {
+            if (FiguresContainer.RocketsList[figure_box.SelectedIndex] != null)
+            {
+                Rocket Rocket = FiguresContainer.RocketsList[figure_box.SelectedIndex];
+                if (int.TryParse(new_X.Text, out int number) && int.TryParse(new_Y.Text, out number))
+                {
+                    Rocket.MoveTo(int.Parse(new_X.Text), int.Parse(new_Y.Text));
+                }
+                else
+                {
+                    MessageBox.Show("Некорректный формат ввода");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Выберите существующую фигуру");
+            }
+        }
+
+        private void figure_box_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            buttonDelete.Enabled = true;
+            Button_New_Cords.Enabled = true;
+            Button_New_Size.Enabled = true;
+            Start_button.Enabled = true;
+        }
+
+        private void Button_New_Size_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(new_height.Text, out int check) && int.TryParse(new_width.Text, out check))
+            {
+                int height = int.Parse(new_height.Text);
+                int width = int.Parse(new_width.Text);
+                if (height > 0 && width > 0)
+                {
+                    Rocket Rocket = FiguresContainer.RocketsList[figure_box.SelectedIndex];
+                    Rocket.ResizeRocket(width, height);
+                }
+                else 
+                {
+                    MessageBox.Show("Габариты ракеты - положительные числа");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ошибка ввода, вводите целые числа");
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            Rocket Rocket = FiguresContainer.RocketsList[figure_box.SelectedIndex];
+            FiguresContainer.RocketsList.Remove(Rocket);
+            Rocket.DeleteF(Rocket, true);
+            figure_box.Items.Clear();
+            figure_box.SelectedIndex = -1;
+            for (int i = 0; i < FiguresContainer.RocketsList.Count; i++)
+            {
+                figure_box.Items.Add(FiguresContainer.RocketsList[i]);
+                figure_box.Items[i] = $"Rocket{FiguresContainer.RocketsList[i].number}";
+            }
+            buttonDelete.Enabled = false;
+            Button_New_Cords.Enabled = false;
+            Button_New_Size.Enabled = false;
+            Start_button.Enabled = false;
+        }
+
+        private void Start_button_Click(object sender, EventArgs e)
+        {
+            Rocket Rocket = FiguresContainer.RocketsList[figure_box.SelectedIndex];
+            Thread fly = new Thread(Rocket.Start_Rocket);
+            fly.Start();
+            if (Rocket.y == 0)
+            {
+                fly.Interrupt();
+            }
+        }
+    }
+}
---
+++ MyFigures/Rocket.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyFigures
+{
+    public class Rocket : Figure
+    {
+        public Triangle Body { get; set; }
+        public Triangle Window { get; set; }
+        public Triangle Left_wing { get; set; }
+        public Triangle Right_wing { get; set; }
+        public Triangle Left_engine { get; set; }
+        public Triangle Middle_engine { get; set; }
+        public Triangle Right_engine { get; set; }
+        public List<Triangle> Rocket_parts { get; set; } = new List<Triangle>();
+        public int left_x, high_y, right_x, low_y;
+        static public int count = 0;
+
+        public Rocket() { }
+        public Rocket(int x, int y, int width, int height)
+        {
+            if (x - width / 10 < 0 || y < 0 || x + width / 10 > pictureBox.Width || y + height * 11 / 10 > pictureBox.Height)
+            {
+                MessageBox.Show("Фигура должна полностью помещаться на холст!");
+            }
+            else
+            {
+                this.x = x; this.y = y; this.width = width; this.height = height;
+                left_x = x - width / 10;
+                high_y = y;
+                right_x = x + width / 10;
+                low_y = y + height * 11 / 10;
+                Create_Body();
+                Create_Window();
+                Create_Left_wing();
+                Create_Right_wing();
+                Create_Left_engine();
+                Create_Middle_engine();
+                Create_Right_engine();
+                FiguresContainer.RocketsList.Add(this);
+                FiguresContainer.figureList.Add(this);
+                number = count;
+                count++;
+            }
+        }
+        private void Create_Body()
+        {
+            Point[] points = new Point[3];
+            points[0] = new Point(x, y + height);
+            points[1] = new Point(x + width, y + height);
+            points[2] = new Point(x + width / 2, y);
+            Body = new Triangle(points, false);
+            Rocket_parts.Add(Body);
+        }
+
+        private void Create_Window()
+        {
+            Point[] points = new Point[3];
+            points[0] = new Point(x + width * 2 / 5, y + height * 3 / 5);
+            points[1] = new Point(x + width * 3 / 5, y + height * 3 / 5);
+            points[2] = new Point(x + width / 2, y + height * 2 / 5);
+            Window = new Triangle(points, false);
+            Rocket_parts.Add(Window);
+        }
+        private void Create_Left_wing()
+        {
+                Point[] points = new Point[3];
+                points[0] = new Point(x + width / 5, y + height * 3 / 5);
+                points[1] = new Point(x + width * 3 / 10, y + height * 2 / 5);
+                points[2] = new Point(x - width / 10, y + height * 3 / 5);
+                Left_wing = new Triangle(points, false);
+                Rocket_parts.Add(Left_wing);
+        }
+
+        private void Create_Right_wing()
+        {
+
+                Point[] points = new Point[3];
+                points[0] = new Point(x + width * 4 / 5, y + height * 3 / 5);
+                points[1] = new Point(x + width * 7 / 10, y + height * 2 / 5);
+                points[2] = new Point(x + width * 11 / 10, y + height * 3 / 5);
+                Right_wing = new Triangle(points, false);
+                Rocket_parts.Add(Right_wing);
+           
+        }
+        private void Create_Left_engine()
+        {
+
+                Point[] points = new Point[3];
+                points[0] = new Point(x + width * 1 / 5, y + height * 9 / 10);
+                points[1] = new Point(x + width * 1 / 10, y + height * 11 / 10);
+                points[2] = new Point(x + width * 3 / 10, y + height * 11 / 10);
+                Left_engine = new Triangle(points, false);
+                Rocket_parts.Add(Left_engine);
+
+        }
+        private void Create_Middle_engine()
+        {
+
+                Point[] points = new Point[3];
+                points[0] = new Point(x + width * 1 / 2, y + height * 9 / 10);
+                points[1] = new Point(x + width * 2 / 5, y + height * 11 / 10);
+                points[2] = new Point(x + width * 3 / 5, y + height * 11 / 10);
+                Middle_engine = new Triangle(points, false);
+                Rocket_parts.Add(Middle_engine);
+
+        }
+        private void Create_Right_engine()
+        {
+                Point[] points = new Point[3];
+                points[0] = new Point(x + width * 4 / 5, y + height * 9 / 10);
+                points[1] = new Point(x + width * 7 / 10, y + height * 11 / 10);
+                points[2] = new Point(x + width * 9 / 10, y + height * 11 / 10);
+                Right_engine = new Triangle(points, false);
+                Rocket_parts.Add(Right_engine);
+        }
+        public override void Draw()
+        {
+            foreach (Triangle part in Rocket_parts)
+            {
+                part.Draw(false);
+            }
+            DrawText("Rocket ", number, x, y + height * 3 / 5, width, height * 2 / 5);
+        }
+        public override void MoveTo(int dx, int dy)
+        {
+            if (x + dx - width / 10 < 0 || y + dy < 0 || x + dx + width / 10 > pictureBox.Width || y + dy + height * 11 / 10 > pictureBox.Height)
+            {
+                MessageBox.Show("Фигура должна полностью помещаться на холст!");
+            }
+            else
+            {
+                x += dx; y += dy;
+                left_x = x - width / 10;
+                high_y = y;
+                right_x = x + width / 10;
+                low_y = y + height * 11 / 10;
+
+                foreach (Triangle part in Rocket_parts)
+                {
+                    for (int i = 0; i < 3; i++)
+                    {
+                        part.points[i].X += dx;
+                        part.points[i].Y += dy;
+                    }
+                }
+                DeleteF(this, false);
+                Draw();
+            }
+        }
+
+        public void ResizeRocket(int width, int height)
+        {
+            if (x - width / 10 < 0 || y < 0 || x + width / 10 > pictureBox.Width || y + height * 11 / 10 > pictureBox.Height)
+            {
+                MessageBox.Show("Фигура должна полностью помещаться на холст!");
+            }
+            else
+            {
+                this.width = width; this.height = height;
+                left_x = x - width / 10;
+                high_y = y;
+                right_x = x + width / 10;
+                low_y = y + height * 11 / 10;
+                Rocket_parts.Clear();
+                Create_Body();
+                Create_Window();
+                Create_Left_wing();
+                Create_Right_wing();
+                Create_Left_engine();
+                Create_Middle_engine();
+                Create_Right_engine();
+                DeleteF(this, false);
+                Draw();
+            }
+
+        }
+
+        public void Start_Rocket()
+        {
+            while (high_y > 5)
+            {
+                MoveTo(0, -5);
+                Task.Delay(50).GetAwaiter().GetResult();
+            }
+            MoveTo(0, high_y);
+        }
+    }
+}
